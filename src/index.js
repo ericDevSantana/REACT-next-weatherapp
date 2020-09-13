0939a7d9caa56a2e9588af5a944587a335fd2ad8
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-13 15:26:37 -0700

    Changes gitignore

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import WeatherIcon from './WeatherIcon';
 ReactDOM.render(
     <div>
     <h1>
-        <WeatherIcon name="cloud" fontSize="Large" color="disabled"/> Weather Forecast</h1>
+            <WeatherIcon name="cloud" fontSize="Large" color="disabled" /> {process.env.API_KEY}</h1>
 
 </div>, document.getElementById('root'));
