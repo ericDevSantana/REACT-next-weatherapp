88aa41fdb27fee87229864e5c98e4bcacc7d6ce2
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-13 12:01:16 -0700

    Font Family, Initial Setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CloudIcon from '@material-ui/icons/Cloud';
 
 ReactDOM.render(
-  <h1>Weather Forecast</h1>,
+  <h1><CloudIcon /> Weather Forecast</h1>,
   document.getElementById('root')
 );
