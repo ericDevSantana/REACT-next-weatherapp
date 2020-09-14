59885398992623c49a365a5f4e821f196ebda545
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-14 08:49:45 -0700

    testing openWeather API

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ function App() {
         fetch("https://api.openweathermap.org/data/2.5/onecall?lat=16.68&lon=49.2&exclude=current,minutely,hourly&appid="+process.env.REACT_APP_API_KEY, {
         })
             .then(response => response.json())
-            .then(data => console.log(data.daily[0].temp.min));
+            .then(data => setData("Max Temperature: "+ Math.floor(data.daily[0].temp.max-273)+" °C"));
     }, []);
 
     return (
