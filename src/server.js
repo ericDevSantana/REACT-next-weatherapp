80fe21be2b13eabf5e61f48f125b43781c5d83db
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-14 08:44:24 -0700

    fetching API

--- src/server.js
+++ src/server.js
@@ -1,19 +1,17 @@
-import React, {Component, useEffect} from "react";
+import React, {Component, useEffect, useState} from "react";
 
 function App() {
+    const [data, setData] = useState("");
     
     useEffect(() => {
-        fetch("http://api.openweathermap.org/data/2.5/forecast?q=nevada&appid="+process.env.REACT_APP_API_KEY, {
-            headers: {
-                'Accept': 'application/json'
-            }
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=16.68&lon=49.2&exclude=current,minutely,hourly&appid="+process.env.REACT_APP_API_KEY, {
         })
             .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => console.log(data.daily[0].temp.min));
     }, []);
 
     return (
-        <h1>Hi</h1>
+        <h1>{data}</h1>
     )
 }
 
