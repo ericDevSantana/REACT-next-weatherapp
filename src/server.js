0b1b978f9ce5f381cb2ac5888f6cbb0db9eec6d9
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-14 15:57:31 -0700

    Getting current weather

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ function App() {
     const [data, setData] = useState("");
     
     useEffect(() => {
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=16.68&lon=49.2&exclude=current,minutely,hourly&appid="+process.env.REACT_APP_API_KEY, {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid="+process.env.REACT_APP_API_KEY, {
         })
             .then(response => response.json())
-            .then(data => setData(Math.floor(data.daily[0].temp.max-273)));
+            .then(data => setData(Math.floor(data.main.temp-273)));
     }, []);
 
     return data;
