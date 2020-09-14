ae6c78d860e0e05194f2b259d3d166a2dce052a7
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-14 15:32:17 -0700

    card style

--- src/WeatherIcon.jsx
+++ src/WeatherIcon.jsx
@@ -5,7 +5,7 @@ import CloudOffIcon from '@material-ui/icons/CloudOff';
 function WeatherIcon(props) {
     switch (props.name) {
         case "cloud":
-            return <CloudIcon style={{ fontSize: 60 , float: "right"}}color={props.color}/>
+            return <CloudIcon style={{ fontSize: 60 , float: "right"}} color={props.color}/>
         default:
             return <CloudOffIcon />
     }
